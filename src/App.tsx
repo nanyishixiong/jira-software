dbb2e5dec7ccc579cced577e6eaaf4a0d0b5b97e
wu guohao <62-wuguohao@users.noreply.gitlab.topviewclub.cn>
2023-01-26 10:41:13 +0800

    feat: 新增登录

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import { ProjectListScreen } from "./screens/project-list";
 import { TestTSHook } from "test_hook";
+import { LoginScreen } from "screens/login/login-screen";
 
 function App() {
   return (
     <div className="App">
-      <TestTSHook></TestTSHook>
+      <LoginScreen></LoginScreen>
     </div>
   );
 }
