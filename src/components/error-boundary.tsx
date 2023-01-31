a3b75d038a4dfe0f5347cbb9da5c440c937dd8bb
wu guohao <62-wuguohao@users.noreply.gitlab.topviewclub.cn>
2023-01-31 12:31:04 +0800

    feat: 添加ErrorBoundary

---
+++ src/components/error-boundary.tsx
@@ -0,0 +1,25 @@
+import React, { Component, ReactNode } from "react";
+
+type FallbackRender = (props: { error: Error | null }) => React.ReactElement;
+// PropsWithChildren React 定义的utils type
+export class ErrorBoundary extends Component<
+  React.PropsWithChildren<{ fallbackRender: FallbackRender }>,
+  { error: Error | null }
+> {
+  state = {
+    error: null,
+  };
+  // 子组件抛出异常，该函数返回的error更新state
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
+
+  render() {
+    const { error } = this.state;
+    const { fallbackRender, children } = this.props;
+    if (error) {
+      return fallbackRender({ error });
+    }
+    return children;
+  }
+}
