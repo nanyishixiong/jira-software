f1e2408ced8e000962be30419cda869c43cf07a4
wu guohao <62-wuguohao@users.noreply.gitlab.topviewclub.cn>
2023-01-31 11:16:39 +0800

    feat: 自定义hook, 抽象请求层，和loading

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,33 +1,36 @@
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
-import { useEffect, useState } from "react";
-import { cleanObject, useMount, useDebounce } from "utils";
-import { useHttp } from "utils/http";
+import { useState } from "react";
+import { useDebounce } from "utils";
 import styled from "@emotion/styled";
+import { Typography } from "antd";
+import { useProject } from "utils/project";
+import { useUser } from "utils/user";
 
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
-  const [users, setUsers] = useState([]);
-  const [list, setList] = useState([]);
   const debounceParam = useDebounce(param, 200);
-  const client = useHttp();
-
-  useEffect(() => {
-    client("projects", { data: cleanObject(debounceParam) }).then(setList);
-  }, [debounceParam]);
-
-  useMount(() => {
-    client("users").then(setUsers);
-  });
+  const { isLoading, error, data: list } = useProject(debounceParam);
+  const { data: users } = useUser();
 
   return (
     <Container>
       <h1>项目列表</h1>
-      <SearchPanel param={param} setParam={setParam} users={users} />
-      <List list={list} users={users} />
+      <SearchPanel param={param} setParam={setParam} users={users || []} />
+      {error ? (
+        <Typography.Text type="danger">{error.message}</Typography.Text>
+      ) : null}
+      <List
+        dataSource={list?.map((item) => {
+          item.key = item.id;
+          return item;
+        })}
+        users={users || []}
+        loading={isLoading}
+      />
     </Container>
   );
 };
---
+++ src/utils/user.ts
@@ -0,0 +1,17 @@
+import { User } from "screens/project-list/search-panel";
+import { useEffect } from "react";
+import { cleanObject } from "utils";
+import { useHttp } from "./http";
+import { useAsync } from "./use-async";
+
+export const useUser = (param?: Partial<User>) => {
+  const client = useHttp();
+  const { run, ...result } = useAsync<User[]>();
+
+  useEffect(() => {
+    run(client("users", { data: cleanObject(param || {}) }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [param]);
+
+  return result;
+};
