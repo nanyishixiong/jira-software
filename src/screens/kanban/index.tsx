18d0b64f98fb18777966db132c1424ef197ea4b0
wu guohao <62-wuguohao@users.noreply.gitlab.topviewclub.cn>
2023-02-09 16:36:23 +0800

    feat: 看板和任务增删改查

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -1,31 +1,49 @@
 import styled from "@emotion/styled";
+import { Spin } from "antd";
 import { ScreenContainer } from "components/lib";
 import { useDocumentTitle } from "utils";
 import { useKanbans } from "utils/kanban";
+import { useTasks } from "utils/task";
+import { CreateKanban } from "./create-kanban";
 import { KanbanColumn } from "./kanban-column";
 import { SearchPanel } from "./search-panel";
-import { useKanbanSearchParams, useProjectInUrl } from "./util";
+import { TaskModal } from "./task-modal";
+import {
+  useKanbanSearchParams,
+  useProjectInUrl,
+  useTasksSearchParams,
+} from "./util";
 
 export const KanbanScreen = () => {
   useDocumentTitle("看板列表");
-  const { data: kanbans } = useKanbans(useKanbanSearchParams());
+  const { data: kanbans, isLoading: kanbanIsLoading } = useKanbans(
+    useKanbanSearchParams()
+  );
   const { data: currentProject } = useProjectInUrl();
+  const { isLoading: taskIsLoading } = useTasks(useTasksSearchParams());
+  const isLoading = taskIsLoading || kanbanIsLoading;
 
   return (
     <ScreenContainer>
       <h1>{currentProject?.name}看板</h1>
       <SearchPanel />
-      <ColumnsContainer>
-        {kanbans?.map((kanban) => (
-          <KanbanColumn key={kanban.id} kanban={kanban} />
-        ))}
-      </ColumnsContainer>
+      {isLoading ? (
+        <Spin size={"large"} />
+      ) : (
+        <ColumnsContainer>
+          {kanbans?.map((kanban) => (
+            <KanbanColumn key={kanban.id} kanban={kanban} />
+          ))}
+          <CreateKanban />
+        </ColumnsContainer>
+      )}
+      <TaskModal />
     </ScreenContainer>
   );
 };
 
-const ColumnsContainer = styled.div`
+export const ColumnsContainer = styled.div`
   display: flex;
-  overflow-x: auto;
+  overflow-x: scroll;
   flex: 1;
 `;
--- src/screens/kanban/util.ts
+++ src/screens/kanban/util.ts
@@ -1,6 +1,7 @@
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { useLocation } from "react-router";
 import { useProject } from "utils/project";
+import { useTask } from "utils/task";
 import { useUrlQueryParam } from "utils/url";
 
 export const useProjectIdInUrl = () => {
@@ -15,12 +16,7 @@ export const useKanbanSearchParams = () => ({ projectId: useProjectIdInUrl() });
 export const useKanbansQueryKey = () => ["kanbans", useKanbanSearchParams()];
 
 export const useTasksSearchParams = () => {
-  const [param, setParam] = useUrlQueryParam([
-    "name",
-    "typeId",
-    "processorId",
-    "tagId",
-  ]);
+  const [param] = useUrlQueryParam(["name", "typeId", "processorId", "tagId"]);
   const projectId = useProjectIdInUrl();
   return useMemo(
     () => ({
@@ -34,3 +30,26 @@ export const useTasksSearchParams = () => {
   );
 };
 export const useTasksQueryKey = () => ["tasks", useTasksSearchParams()];
+
+export const useTasksModal = () => {
+  const [{ editingTaskId }, setEditingTaskId] = useUrlQueryParam([
+    "editingTaskId",
+  ]);
+  const { data: editingTask, isLoading } = useTask(Number(editingTaskId));
+  const startEdit = useCallback(
+    (id: number) => {
+      setEditingTaskId({ editingTaskId: id });
+    },
+    [setEditingTaskId]
+  );
+  const close = useCallback(() => {
+    setEditingTaskId({ editingTaskId: "" });
+  }, [setEditingTaskId]);
+  return {
+    editingTaskId,
+    editingTask,
+    startEdit,
+    close,
+    isLoading,
+  };
+};
